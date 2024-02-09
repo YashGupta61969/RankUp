89fce6969c57b9348d6bbfbac249430649cb3875
YashGupta61969 <yashgupta61969@gmaill.com>
2024-02-09 11:58:57 +0530

    Application Name change to RankUp

--- android/app/src/main/java/com/simpeval/MainApplication.kt
+++ android/app/src/main/java/com/simpeval/MainApplication.kt
@@ -1,4 +1,4 @@
-package com.simpeval
+package com.RankUp
 
 import android.app.Application
 import com.facebook.react.PackageList
--- ios/SimpEvalTests/SimpEvalTests.m
+++ ios/SimpEvalTests/SimpEvalTests.m
@@ -7,11 +7,11 @@
 #define TIMEOUT_SECONDS 600
 #define TEXT_TO_LOOK_FOR @"Welcome to React"
 
-@interface SimpEvalTests : XCTestCase
+@interface RankUpTests : XCTestCase
 
 @end
 
-@implementation SimpEvalTests
+@implementation RankUpTests
 
 - (BOOL)findSubviewInView:(UIView *)view matching:(BOOL (^)(UIView *view))test
 {
